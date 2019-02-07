fce671d2617c725df6599d521889b7bc6a58d31d
Trey <tbeau5595@hotmail.com>
2019-02-07 20:55:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -28,13 +28,13 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
